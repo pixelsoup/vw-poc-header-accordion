9cc87e12798158c262536fdb084906eb45b8b04f
Geoff Schwartz <eieio@pixelsoup.com.au>
2021-07-10 13:29:42 +1000

    Accordion - change javascript to reusable function

--- includes/js/accordion.js
+++ includes/js/accordion.js
@@ -2,19 +2,19 @@
 // https://codeorum.com/tutorials/simple-accordion-collapsible-element-with-vanilla-js-and-simple-animation-effect
 const accordionFunction = (accTargetElement, isOpenOnlyOne) => {
 
-  document.addEventListener('click', (clickHandler) => {
-    if (!clickHandler.target.matches(accTargetElement + ' .js-accBtn')) return
+  document.addEventListener('click', (handleClick) => {
+    if (!handleClick.target.matches(accTargetElement + ' .js-accBtn')) return
     else {
-      if (!clickHandler.target.parentElement.classList.contains('expanded')) {
+      if (!handleClick.target.parentElement.classList.contains('expanded')) {
         if (isOpenOnlyOne == true) {
           const elementList = document.querySelectorAll(accTargetElement + ' .js-accPanel')
-          Array.prototype.forEach.call(elementList, (clickHandler) => {
-            clickHandler.classList.remove('expanded')
+          Array.prototype.forEach.call(elementList, (handleClick) => {
+            handleClick.classList.remove('expanded')
           })
         }
-        clickHandler.target.parentElement.classList.add('expanded')
+        handleClick.target.parentElement.classList.add('expanded')
       } else {
-        clickHandler.target.parentElement.classList.remove('expanded')
+        handleClick.target.parentElement.classList.remove('expanded')
       }
     }
   })
